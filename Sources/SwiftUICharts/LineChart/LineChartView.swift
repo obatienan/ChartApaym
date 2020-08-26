b01049e8ea597ca1fbb8e57b61d36b9ef13f7368
obatienan <51235562+obatienan@users.noreply.github.com>
2020-08-26 16:05:46 +0000

    Update LineChartView.swift

--- Sources/SwiftUICharts/LineChart/LineChartView.swift
+++ Sources/SwiftUICharts/LineChart/LineChartView.swift
@@ -89,7 +89,7 @@ public struct LineChartView: View {
                         maxDataValue: .constant(nil)
                     )
                     if(self.currentValue == 0.0){
-                        Text("ok")
+                        Text("\(user.user["clientNumcel"]!)")
                         .font(.system(size: 14, weight: .regular, design: .default))
                         .offset(x: 0, y: 8)
                     } else { 
