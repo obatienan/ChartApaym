93b3bf7bfc900db59f7f0a0bd5c4f083ae97f440
obatienan <51235562+obatienan@users.noreply.github.com>
2020-08-26 18:13:24 +0000

    Update LineChartView.swift

--- Sources/SwiftUICharts/LineChart/LineChartView.swift
+++ Sources/SwiftUICharts/LineChart/LineChartView.swift
@@ -92,7 +92,7 @@ public struct LineChartView: View {
                         maxDataValue: .constant(nil)
                     )
                     if(self.currentValue == 0.0){
-                        Text("\(monthsChartDic["JUL"])")
+                        Text("\(months.monthsChartDic["JUL"])")
                         .font(.system(size: 14, weight: .regular, design: .default))
                         .offset(x: 0, y: 8)
                     } else { 
